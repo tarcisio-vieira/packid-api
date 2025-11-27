2a9f4c7efbf6edb1e18b6d0f25cbcb404f60c4ff
Tarcisio Vieira <tarcisio@outlook.com.br>
2025-11-26 21:06:00 -0300

    Initial commit: PackID API project

---
+++ src/main/java/com/packid/api/PackIdApiApplication.java
@@ -0,0 +1,13 @@
+package com.packid.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackIdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackIdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packid/api/PackIdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packid.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackIdApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
