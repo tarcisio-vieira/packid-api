6650039935c04fbb2f6b93490307a11542bdc85d
TARCISIO GARCIA VIEIRA <tarcisio.vieira@gsw.com.br>
2026-08-17 19:22:42 -0300

    Clean code

---
+++ src/main/java/com/packid/api/config/StaticResourceCacheConfig.java
@@ -0,0 +1,19 @@
+package com.packid.api.config;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class StaticResourceCacheConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/static/assets/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic().immutable());
+    }
+}
